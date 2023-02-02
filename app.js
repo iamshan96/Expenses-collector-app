4d249168d3c892d80ec5fdfe7f9e65b475341d29
iamshan96 <124255638+iamshan96@users.noreply.github.com>
2023-02-02 19:58:34 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,93 @@
+import { v4 as uuidv4 } from "https://jspm.dev/uuid";
+
+const expensesContainer = document.getElementById("expenses-container");
+const inputBtn = document.getElementById("input-btn");
+const inputTextBox = document.getElementById("input-text-box");
+const inputAmountBox = document.getElementById("input-amount-box");
+
+let expensesListArray = [];
+
+const dataFromLocalStorage = JSON.parse(
+  localStorage.getItem("expensesListArray")
+);
+
+if (dataFromLocalStorage.length > 0) {
+  expensesListArray = dataFromLocalStorage;
+  render();
+}
+
+document.addEventListener("click", function (e) {
+  if (e.target.dataset.remove) {
+    removeItemClick(e.target.dataset.remove);
+  } else if (e.target.id === "input-btn") {
+    inputBtnHandleClick();
+  }
+});
+
+function removeItemClick(tweetId) {
+  const targetObj = expensesListArray.filter(function (expense) {
+    return expense.uuid === tweetId;
+  })[0];
+
+  const checkIndex = expensesListArray.indexOf(targetObj);
+  expensesListArray.splice(checkIndex, 1);
+  localStorage.setItem("expensesListArray", JSON.stringify(expensesListArray));
+  render();
+  if (expensesListArray.length === 0) {
+    expensesContainer.innerHTML = "";
+  }
+}
+
+function inputBtnHandleClick() {
+  if (inputTextBox.value && inputAmountBox.value) {
+    expensesListArray.push({
+      name: inputTextBox.value,
+      amount: inputAmountBox.value,
+      uuid: uuidv4(),
+    });
+    localStorage.setItem(
+      "expensesListArray",
+      JSON.stringify(expensesListArray)
+    );
+
+    render();
+  }
+  inputTextBox.value = "";
+  inputAmountBox.value = "";
+}
+
+function getExpensesHtml() {
+  let count = 0;
+  let totalExpenses = 0;
+  let expensesHtml = "";
+  expensesListArray.forEach(function (expense) {
+    totalExpenses += Number(expense.amount);
+    count++;
+    expensesHtml += `
+        <div class='expenses-list-item'>
+          <div class='list-item-left'>
+            ${count}) ${expense.name}
+            <i class="remove-item fa-solid fa-xmark" data-remove='${expense.uuid}'></i>
+          </div>
+            Rs. ${expense.amount}
+        </div>`;
+  });
+
+  return `
+  <div id="expenses-list" class="expenses-list">
+    ${expensesHtml}
+  </div>
+  <div class='total-expenses'>
+    <span>
+      Total:
+    </span>
+    <span>
+      Rs. ${totalExpenses}
+    </span>
+  </div>
+  `;
+}
+
+function render() {
+  expensesContainer.innerHTML = getExpensesHtml();
+}
